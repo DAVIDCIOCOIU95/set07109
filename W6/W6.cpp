8132acc0111eca7774aa8626e576a08383ca0de7
DAVIDCIOCOIU95 <david.samir.ciocoiu@gmail.com>
2019-03-05 11:21:18 +0000

    new

--- W6/W6.cpp
+++ W6/W6.cpp
@@ -1,9 +1,11 @@
- #include <iostream>
- #include <cstdlib>
- #include <cstdio>
+/*
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <memory>
 
 using namespace std;
-
+*/
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 /*
 // calloc and malloc
@@ -122,13 +124,14 @@ int main(int argc, char **argv)
 */
 
 //6.14 new and delete
-/**/
+/*
 
 // Note that here we do not create an array but allocate space for one int
 int *foo()
 {
 	// Create memory on the heap
 	int *data = new int(5);
+	cout << "Address of foo one value: " << data << ",value: " << *data << endl;
 	
 	return data;
 }
@@ -150,6 +153,7 @@ int main(int argc, char **argv)
 	// Call foo
 	int *data = foo();
 	// Output address and value
+	// data stores the address of *data in foo; therefore *data will be a pointer do data in foo 
 	cout << "Address = " << data << ", value = " << *data << endl;
 	// Delete the memory
 	delete data;
@@ -163,10 +167,308 @@ int main(int argc, char **argv)
 	delete[] data2;
 	return 0; 
 }
+*/
 
+//-----------------------------------------------------------------------------------
 
+// 6.16 Using shared_ptr
+/*
+void foo(shared_ptr<int> value)
+{
+	// Print out number of accessors ot the shared object - should be 2 
+	cout << "In foo, count on shared_ptr = " << value.use_count() << endl;
+	// shared_ptr behaves like a pointer - we can assign to it by deferencing 
+	*value = 5; 
+}
 
+int main(int argc, char **argv)
+{
+	// We can create a shared_ptr by wrapping it around an allocated object
+	shared_ptr<int> value = shared_ptr<int>(new int(10));
+	cout << "Initial address = " << value << ", value = " << *value << endl;
+	// We can also create a shared_ptr by using the make_shared function
+	value = make_shared<int>(70);
+	cout << "After make shared, address = " << value << ",value = " << *value << endl;
+	// Print out number of accessors tot he shared object 
+	cout << "Before foo, count on shared_ptr = " <<  value.use_count() << endl;
+	// Let's call foo
+	foo(value);
+	cout << "After foo, address = " << value << ", value = " << *value << endl;
+	// Print out number of accessors to the shared object 
+	cout << "After foo, count on shared_ptr = " << value.use_count() << endl;
+	// Let's set value to nullptr - this will clear the memory 
+	value = nullptr;
+	// Print out number of accessors to the shared object. Should be 0
+	cout << "After nullptr, count on shared_ptr = " << value.use_count() << endl;
+	
+	return 0;
+}
+*/
 
+// --------------------------------------------------------------------
 
+// 6. 18 Examining the shared_ptr Value 
+/*
+void foo(shared_ptr<int> value)
+{
+	// Output number of accessors to value - 2 
+	cout << "In foo, accessors to value = " << value.use_count() << endl;
+	// Call make_shared again - we lose this copy
+	value = make_shared<int>(25);
+	// Output number of accessors to value - should be 1 
+	cout << "In foo, after make_shared, accessors to value = " << value.use_count() << endl;
+	// No memory leaks! shared_ptr will clean itself up
+}
 
+// Note we now have a reference to the shared_ptr
+void foo2(shared_ptr<int> &value)
+{
+	// Output number of accessors to value - should be 1 
+	cout << "In foo 2, accessor to value = " << value.use_count() << endl;
+	// Call make_shared again - we retain this copy
+	value = make_shared<int>(100);
+	// Output number of accessors to value - should still be 2 
+	cout << "In foo2, after make_shared, accessors to value = " << value.use_count() << endl;
+}
 
+int main(int argc, char **argv)
+{
+	// Create a new shared_ptr
+	shared_ptr<int> value = make_shared<int> (5);
+	// Print data
+	cout << "Address = " << value << ", value = " << *value << ", usage = " << value.use_count() << endl;
+	// Call foo
+	foo (value);
+	// Print data
+	cout << "Address = " << value << ", value = " << *value << ", usage = " << value.use_count() << endl;
+	// Call foo2
+	foo2(value);
+	// Print data
+	cout << "Address = " << value << ", value = " << *value << ", usage = " << value.use_count() << endl;
+
+	return 0; 
+}
+*/
+
+// -------------------------------------------------------------------
+// 6.12 Using shared_ptr for Arrays 
+/*
+const int SIZE = 65536;
+
+void build_array(shared_ptr<int> data)
+{
+	for (int i = 0; i < SIZE; ++i)
+		// Have to deference the pointer, then access the values
+		data.get()[i] = i;
+}
+
+void print_array(shared_ptr<int> data)
+{
+	for (int i = 0; i < SIZE; ++i)
+		// Have to deference the pointer, then access the values
+		cout << data.get()[i] << endl;
+}
+
+int main(int argc, char **argv)
+{
+	// We have to tell shared_ptr to delete the data
+	// When creating use the default_delete function
+	shared_ptr<int> data = shared_ptr<int>(new int[SIZE], default_delete<int[]>());
+	// Build the array
+	build_array(data);
+	// Print the array
+	print_array(data);
+	// Set data to nullptr - will cause data to be deleted
+	data = nullptr; 
+
+	return 0;
+}
+*/
+
+//------------------------------------
+// 6.21 Using unique_ptr
+/*
+void foo(unique_ptr<int> value)
+{
+	// Let's set the value to 500. Can treat just as a pointer
+	*value = 500;
+	// We don't return unique_ptr. We have lost *ALL* the data
+}
+
+unique_ptr<int> foo2(unique_ptr<int> value)
+{
+	// This time we will move the answer back
+	*value = 500;
+	return value; 
+}
+
+void foo3(unique_ptr<int> &value)
+{
+	// Using a reference solves the problem 
+	*value = 100;
+}
+
+int main(int argc, char **argv)
+{
+	// Create a unique_ptr value
+	unique_ptr<int> value (new int(50));
+	// Print data
+	cout << "Address = " << value.get() << ", value = " << *value << endl;
+	// Call foo. We have to "move" the pointer into foo
+	foo(move(value));
+	// Print data. Cannot print value. No longer valid. 
+	cout << "After foo, address = " << value.get() << endl;
+
+	// Recreate the value
+	value = unique_ptr<int>(new int(500));
+	// Print data
+	cout << "Address = " << value.get() << ",value = " << *value << endl;
+	// Call foo2. We have to "move" the pointer into foo
+	value = foo2(move(value));
+	// Print data 
+	cout << "After foo2, address = " << value.get() << ", value = " << *value << endl;
+
+	// Call foo3. Use a reference to avoid a move 
+	foo3(value);
+	cout << "After foo3, address = " << value.get() << ", value = " << *value << endl;
+
+	return 0; 
+}
+*/
+
+// -----------------------------------------------
+// 6.23 Using unique_ptr for Arrays
+/*
+const int SIZE = 65536;
+
+void build_array(unique_ptr<int[]> &data)
+{
+	for (int i = 0; i < SIZE; ++i)
+		// Have to deference the pointer, the access the values
+		data.get()[i] = i;
+}
+
+void print_array(unique_ptr<int[]> &data)
+{
+	for (int i = 0; i < SIZE; ++i)
+		// Have to deference the pointer, the access the values
+		cout << data.get([i] << endl;
+}
+
+int main (int argc, char** argv)
+{
+	// unique_ptr can have arrays 
+	unique_ptr<int[]> data(new int[SIZE]);
+	// Build the array - using pass by reference on the unique_ptr
+	build_array(data);
+	// Print the array
+	print_array(data);
+	// Set data to nullptr - will delete the array
+	data - nullptr;
+
+	return 0; 
+}
+*/
+//-----------------------------------
+// The auto Keyword (From now on will copy each bit of code)
+// 6.21 Using auto to Simplify Code
+/*
+#include <iostream> 
+#include <memory>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	// Use the auto keywork - compiler determines the type
+	auto data = make_shared<int>(5);
+	// ... data is a shared_ptr<int>  Compiler fills in the type
+	cout << "My data = " << *data << ", at address = " << data << endl;
+	return 0;
+}
+*/
+//-------------------------------
+// 6.25 Using static_cast (outputs error)
+/*
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	// Declare an int 
+	int x = 10; 
+	// Cast to a float 
+	// Let the compiler work out the type
+	auto y = static_cast<float>(x);
+	// Declare a string
+	string str = "Hello World!";
+	// Cast str to a float 
+	// Will cause a compiler error
+	auto z = static_cast<float>(str);
+
+	return 0;
+}
+
+*/
+//-----------------------------------------------------------------
+// 6.27 Casting with const_cast 
+/*
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	// Create a string
+	string str = "Hello World!";
+	cout << "String = " << str << endl;
+	// Get the raw C string
+	const char *const_raw = str.c_str();
+	// Can't chance any values in raw - const 
+	// This won't compile 
+	// const_raw[4] = 'p';
+	
+	// Now let's live dangeriously - use const cast
+	char *raw = const_cast<char*>(const_raw);
+	// Change the value of raw now
+	raw[4] = 'p';
+	// Print out the string
+	cout << "String = " << str << endl;
+
+	return 0;
+}
+*/
+//---------------------------------------------------------
+// 6.29 Using typeid
+/*
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+	// Create a string
+	string str1 = "Hello";
+	string str2 = "World";
+
+	// Check if the types are equal
+	if (typeid(str1) == typeid(str2))
+		cout << "Types are equal" << endl;
+	else
+		cout << "Types are not equal" << endl;
+
+	// Create a float
+	float x = 52.0f;
+
+	// Print out details
+	cout << "x is a " << typeid(x).name() << " with value " << x << endl;
+
+	return 0;
+}
+*/
+//------------------------------------------------------------------------
+// Case Study - Building Trees : see other file
